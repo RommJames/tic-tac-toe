f58e4f43715115796d4547e935ed1d63c463aaca
RommJames <rj.cuya010@gmail.com>
2024-11-08 09:17:26 +0800

    Update X and O in the gameboard

--- src/script.js
+++ src/script.js
@@ -6,34 +6,82 @@ const gameboard = (function(){
     ]
 })();
 
-const playGame = function(symbols, position){
-    
-    const pos = position
+// Check Winner
+// function checkWinner(){
+//     for(let count = 0; count < gameboard.length; count++){
+//         const getRow = gameboard[count]       
+//         // get left to right diagonal
+//         let count = 0; count < gameboard.length; count++
+//     }
+// }
+// checkWinner()
 
+// Start Game
+function playGame(){    
+    let symbol = "X";
+
+    function getMoves(){
+        const position = findPosition(+prompt("Choose Moves3"));
+        gameboard[position.row][position.getPos] = symbol;        
+        symbol = symbol === "X" ? "O" : "X";
+        // return symbol
+    }    
+
+    const updateBoard = ()=> gameboard;
+    
+    return {getMoves, updateBoard}
 }
 
+
+// Get Position of Selected Cell
 const findPosition = function(position){
 
-    for(let count = 0; count < gameboard.length; count++){
-        const getRow = gameboard[count]        
-        const getPos = gameboard[count].indexOf(position);
+    for(let row = 0; row < gameboard.length; row++){         
+        const getPos = gameboard[row].indexOf(position);
         if(getPos != -1){
-            console.log("Count: ", count);
+            console.log("Row: ", row);
             console.log("Pos: ", getPos);
-            return
+            return {row, getPos}                        
         }
-
-        // for(let col = 0; col < getRow.length; col++){
-        //     let getCol = gameboard[count][col];            
-            
-        //     // console.log("Value: ",getCol);
-        // }
 
     }
 
 }
+const getPlay = playGame()
 
-findPosition(5)
+function startGame(){
+    do{
+       getPlay.getMoves();
+        console.log(getPlay.updateBoard());
+    }
+    while(prompt("Continue Game?").toUpperCase() == "Y")
+}
+
+startGame()
+
+
+// Testing Area
+
+// function score(){
+//     let pts = 0;
+//     let symbol = "x"
+
+//     function getScore(){        
+//         pts += 1;  
+//         symbol = symbol === "X" ? "O" : "X";
+//         return {pts,symbol};        
+//     }
+//     return {getScore}
+// }
+
+// const updateScore = score()
+
+// do{
+//     console.log(updateScore.getScore());
+// }
+// while(prompt("Continue Game?").toUpperCase() == "Y")
+
+
 
 // const printGameboard = function(){
 //     for(let count = 0; count < gameboard.length; count++){
